4baa1c4120e92d0be04c5b57fa1739b014c58c62
iowa329 <iowa329@gmail.com>
2022-09-01 00:45:04 +0900

    2주차 수요일 백준6603번 구현완료

--- practice/src/baekjoon/Baekjoon6603.java
+++ practice/src/baekjoon/Baekjoon6603.java
@@ -4,70 +4,93 @@ import java.util.*;
 
 public class Baekjoon6603 {
 
+	static int[] arr = new int[6];
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		// 여러개의 테스트 케이스 받기
 		while(true) {
 			int k = sc.nextInt();
 			if(k == 0) break;
 			
+			// 배열값 입력받기
 			int[] lotto = new int[k];
 			for(int i=0; i<k; i++) {
 				lotto[i] = sc.nextInt();
 			}
 			System.out.println("\n입력받은 배열: " + Arrays.toString(lotto));
 			
-			// first line
-//			for(int i=0; i<k; i++) {
-//				System.out.print(lotto[i] + " ");				
-//			}
-//			System.out.println();
-			
-			// start pattern
-//			int depth = k-6;
-//			int eraseCnt = 1;
-
-			
-			int conquer = 0;
-			int depth = k-6;
-			int depthCnt = 0;
-			while(conquer < 6) {
-					
-				
-				
-				
-				
-//				System.out.println(">>> " + conquer + " " + depthCnt);
-				
-				for(int i=0; i<6; i++) {
-					if (i >= 5-conquer) {
-						if (i != 5) { // 마지막 인덱스가 아닌 경우에만
-							System.out.print(lotto[i+1] + " ");							
-						} else {
-							System.out.print(lotto[i+depthCnt] + " ");
-							depthCnt++;
-						}
-					} else {
-						System.out.print(lotto[i] + " ");				
-					}
-					
-				}
-				System.out.println();
-				
-				
-				if(depthCnt > depth) {
-					depthCnt = 1;
-					conquer++;
-					System.out.println();
-				}
-				
-			}
-			
-			
-			
+			// lotto 순회(DFS 재귀호출)
+			arrangement(0, 0, lotto, k);
+			System.out.println();
 		}
 		sc.close();
 		
 	}
+	
+	private static void arrangement(int startIndex, int depth, int[] lotto, int k) {
+		// 탐색조건 만족(순회하여 6번째 재귀)시 출력
+		if(depth == 6) {
+			for(int i=0; i<6; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=startIndex; i<k; i++) {
+			arr[depth] = lotto[i];
+			arrangement(i+1, depth+1, lotto, k);
+		}
+	}
 
 }
+
+
+//제출코드
+
+//import java.util.*;
+//
+//public class Main {
+//
+//	static int[] arr = new int[6];
+//	
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		while(true) {
+//			int k = sc.nextInt();
+//			if(k == 0) break;
+//			
+//			// 배열 입력
+//			int[] lotto = new int[k];
+//			for(int i=0; i<k; i++) {
+//				lotto[i] = sc.nextInt();
+//			}
+//			
+//			// lotto 순회(DFS 재귀호출)
+//			arrangement(0, 0, lotto, k);
+//			System.out.println();
+//		}
+//		sc.close();
+//		
+//	}
+//	
+//	private static void arrangement(int startIndex, int depth, int[] lotto, int k) {
+//		// 탐색조건 만족(순회하여 6번째 재귀)시 출력
+//		if(depth == 6) {
+//			for(int i=0; i<6; i++) {
+//				System.out.print(arr[i] + " ");
+//			}
+//			System.out.println();
+//			return;
+//		}
+//		
+//		for(int i=startIndex; i<k; i++) {
+//			arr[depth] = lotto[i];
+//			arrangement(i+1, depth+1, lotto, k);
+//		}
+//	}
+//
+//}
