214a3be5bcca868012bf048b3ffb47ce54cd6b2b
iowa329 <iowa329@gmail.com>
2022-09-06 20:57:43 +0900

    3주차 월요일 백준16928번 구현

--- practice/src/baekjoon/Baekjoon16928.java
+++ practice/src/baekjoon/Baekjoon16928.java
@@ -1,40 +1,55 @@
 package baekjoon;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-
-import org.graalvm.compiler.core.common.cfg.Loop;
-import org.graalvm.compiler.nodes.graphbuilderconf.InvocationPlugins.InvocationPluginReceiver;
+import java.util.*;
 
 public class Baekjoon16928 {
 
+	// 편의상 0번 인덱스는 제외
+	static int[] boardInfo = new int[100+1];
+	static boolean[] visited = new boolean[100+1];
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 				
 		int cntLadder = sc.nextInt();
 		int cntSnake = sc.nextInt();
-		
-		// 편의상 0번 인덱스는 제외
-		int[] boardInfo = new int[100+1];
-		boolean[] visited = new boolean[100+1];
 
 		// 입력값 받기
 		for(int i=0; i<cntLadder; i++) {
 			int position = sc.nextInt();
-			boardInfo[position] = sc.nextInt(); 
+			boardInfo[position] = sc.nextInt();
 		}
 		for(int i=0; i<cntSnake; i++) {
 			int position = sc.nextInt();
-			boardInfo[position] = -(sc.nextInt());
+			boardInfo[position] = sc.nextInt();
 		}
+		
+		// 보드값 입력 검사
+//		for(int j=0; j<100; j=+10) {
+//			for(int i=1; i<=10; i++) {
+//			
+//				if(boardInfo[i+j] == 0) System.out.print("0");
+//				System.out.print(boardInfo[i+j] + " ");
+//			}
+//			System.out.println();
+//			
+//		}
+//		for(int i=0; i<101; i++) {
+//			System.out.print(boardInfo[i]+ " ");
+//		}
+		
+		
 		sc.close();
 
-		// BFS
-		int cntDice = 0;
-		
+		// 보드게임 시작
+		int result = playGame();
+		System.out.println(result);
+	}
+
+	//BFS
+	private static int playGame() {
+//		int cntDice = 0;
 		
 		Queue<Player> que = new LinkedList<Player>();
 		que.add(new Player(1, 0));
@@ -48,6 +63,9 @@ public class Baekjoon16928 {
 				int movePosition = currentPosition.position + dice;
 				int moveCntDice = currentPosition.cntDice + 1;
 				
+				
+				System.out.println(movePosition + " " + moveCntDice);
+				
 				// 보드판 범위를 넘어서거나 이미 방문한 경우 제외
 				if(movePosition > 100 ||
 					visited[movePosition] == true) {
@@ -56,20 +74,27 @@ public class Baekjoon16928 {
 				
 				// 목적지에 도착한 경우
 				if(movePosition == 100) {
-					System.out.println(cntDice);
-					break;
+					return moveCntDice;
 				}
 				
+				visited[movePosition] = true;
 				// 보드의 정보(사다리와 뱀)를 읽어와 위치를 계산
-				visited[currentPosition] = true;
-				currentPosition += boardInfo[currentPosition];
-				que.add(currentPosition);
+				if(boardInfo[movePosition] != 0) {
+					movePosition = boardInfo[movePosition];
+				}
+				que.add(new Player(movePosition, moveCntDice));					
+				
+				
 			}
-			
+			System.out.println();
 		}
+		
+		return 0;
 	}
-
+	
 }
+
+
 
 class Player {
 	int position;
