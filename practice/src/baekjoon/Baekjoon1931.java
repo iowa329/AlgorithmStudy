aad16a7e9355889a301ce291c522bb8a70f7f534
iowa329 <iowa329@gmail.com>
2022-09-13 20:44:50 +0900

    4주차 월요일 백준1931번 구현

--- practice/src/baekjoon/Baekjoon1931.java
+++ practice/src/baekjoon/Baekjoon1931.java
@@ -1,101 +1,108 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Baekjoon1931 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int cnt = sc.nextInt();
+		int n = sc.nextInt();
 		
 		// 일정표 입력 받기
-		int[][] meeting = new int[cnt][2];		
-		for(int i=0; i<cnt; i++) {
-			meeting[i][0] = sc.nextInt();
-			meeting[i][1] = sc.nextInt();
+		int[][] meeting = new int[n][2];		
+		for(int i=0; i<n; i++) {
+			meeting[i][0] = sc.nextInt(); // 시작시간
+			meeting[i][1] = sc.nextInt(); // 종료시간
 		}
 		sc.close();
 		
-//		for(int i=0; i<cnt; i++) {
-//			timeTable = meeting[i][1];
-//			System.out.print("일정" + (i+1) + ": " + meeting[i][0] + " " + meeting[i][1]);
-//			for(int j=i; j<cnt; j++){
-//				if(meeting[j][0] >= timeTable) {
-//					timeTable = meeting[j][1];
-//					System.out.print(" --> " + meeting[j][0] + " " + meeting[j][1]);
-//					meets++;
-//				}
-//			}
-//			schedule[i] = meets;
-//			System.out.println(" // 잡은 일정 개수 : " + schedule[i] + " ");
-//			meets = 1;
-//			if(schedule[i] > bestSchedule)
-//				bestSchedule = schedule[i];
-//		}
 		
-		
-		int hours = 0;
-		int pos = 0;
-		// 입력받은 일정표 정렬하기
-		while(hours<24) {
-
-			while(pos<cnt) {
-				for(int i=0; i<cnt; i++) {
-					if(meeting[i][0] == hours) {
-						int tmp = meeting[pos][0];
-						meeting[pos][0] = meeting[i][0];
-						meeting[i][0] = tmp;
-						System.out.print(meeting[i][0] + " ");
-						i = cnt;
-					}
+		// 입력받은 일정표 종료시간 기준으로 정렬
+		Arrays.sort(meeting, new Comparator<int[]>() {
+			// 인터페이스인 comparator를 오버라이드 하여 구현
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				// 만약 배열의 두번쨰 원소([1] -> 종료시간)가 같다면
+				if(o1[1] == o2[1]) { 
+					// 첫번쨰 원소([0] -> 시작시간)을 오름차순 정렬(수식의 결과값이 음수이면 선행원소가 작다는 의미로 오름차순이다. 값을 바꾸지 않는다)
+					return o1[0] - o2[0]; 
+				} else {
+					// 그 외의 경우에는 종료시간 기준으로 오름차순 정렬
+					return o1[1] - o2[1]; 
 				}
-				pos++;
 			}
-			
-			hours++;
+		});
+		
+		
+		// Greedy
+		int cnt = 1;
+		int beforeEndTime = meeting[0][1];
+		for(int i=1; i<n; i++) {
+			int nextStartTime = meeting[i][0];
+			if(nextStartTime >= beforeEndTime) {
+				cnt++;
+				beforeEndTime = meeting[i][1]; // nextStartTime의 종료시간으로 갱신
+			}
 		}
 		
 		
-		for(int i=0; i<cnt; i++) {
-			System.out.print(meeting[i][0] + " ");
-			System.out.println(meeting[i][1]);
-		}
-		
-//		int bestSchedule = 0;
-//		int timeTable = 0;
-//		int meets = 0;
-//		
-//		hours = 0;
-//		while(hours<24) {
-//			
-//			for(int i=0; i<cnt; i++) {
-//				if(hours == meeting[i][0]) {
-//					timeTable = meeting[i][1];
-//					meets++;
-//					System.out.print(hours + "hours: " + meeting[i][0] + " " + meeting[i][1]);
-//					
-//					for(int j=0; j<cnt; j++){
-//						if(meeting[j][0] >= timeTable) {
-//							timeTable = meeting[j][1];
-//							System.out.print(" --> " + meeting[j][0] + " " + meeting[j][1]);
-//							meets++;
-//							
-//							
-//						}
-//					}
-//					System.out.println(" // 잡은 일정 개수 : " + meets + " ");
-//					if(meets > bestSchedule)
-//						bestSchedule = meets;
-//					meets = 0;
-//				}
-//
-//			}
-//			hours++;
-//			
-//		}
-//		
-//		System.out.println("");
-//		System.out.println(bestSchedule);
+		// 배정결과 최대 회의개수
+		System.out.println(cnt);
 	}
 
 }
+
+
+// 제출코드
+
+//import java.util.*;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		
+//		// 일정표 입력 받기
+//		int[][] meeting = new int[n][2];		
+//		for(int i=0; i<n; i++) {
+//			meeting[i][0] = sc.nextInt(); // 시작시간
+//			meeting[i][1] = sc.nextInt(); // 종료시간
+//		}
+//		sc.close();
+//		
+//		
+//		// 입력받은 일정표 종료시간 기준으로 정렬
+//		Arrays.sort(meeting, new Comparator<int[]>() {
+//			// 인터페이스인 comparator를 오버라이드 하여 구현
+//			@Override
+//			public int compare(int[] o1, int[] o2) {
+//				// 만약 배열의 두번쨰 원소([1] -> 종료시간)가 같다면
+//				if(o1[1] == o2[1]) { 
+//					// 첫번쨰 원소([0] -> 시작시간)을 오름차순 정렬(수식의 결과값이 음수이면 선행원소가 작다는 의미로 오름차순이다. 값을 바꾸지 않는다)
+//					return o1[0] - o2[0]; 
+//				} else {
+//					// 그 외의 경우에는 종료시간 기준으로 오름차순 정렬
+//					return o1[1] - o2[1]; 
+//				}
+//			}
+//		});
+//		
+//		
+//		// Greedy
+//		int cnt = 1;
+//		int beforeEndTime = meeting[0][1];
+//		for(int i=1; i<n; i++) {
+//			int nextStartTime = meeting[i][0];
+//			if(nextStartTime >= beforeEndTime) {
+//				cnt++;
+//				beforeEndTime = meeting[i][1]; // nextStartTime의 종료시간으로 갱신
+//			}
+//		}
+//		
+//		
+//		// 배정결과 최대 회의개수
+//		System.out.println(cnt);
+//	}
+//
+//}
