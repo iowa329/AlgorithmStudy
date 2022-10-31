4b16ab46bc8508637d8b17f409d23370cdc821c3
iowa329 <iowa329@gmail.com>
2022-10-31 19:56:59 +0900

    구조 간소화

--- practice/src/baekjoon/Baekjoon14425.java
+++ practice/src/baekjoon/Baekjoon14425.java
@@ -13,15 +13,10 @@ public class Baekjoon14425 {
 		int m = Integer.parseInt(strTkr.nextToken());
 		
 		HashMap<String, Integer> hashMap = new HashMap<>();
-		
-		for(int i=0; i<n; i++) {
-			hashMap.put(br.readLine(), 0);	
-		}
+		for(int i=0; i<n; i++) hashMap.put(br.readLine(), 0);
 		
 		int answer = 0;
-		for(int i=0; i<m; i++) {
-			if(hashMap.get(br.readLine()) != null) answer++;
-		}
+		for(int i=0; i<m; i++) if(hashMap.get(br.readLine()) != null) answer++;
 		br.close();
 		
 		System.out.println(answer);
