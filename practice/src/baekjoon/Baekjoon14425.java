1ed27d4916d29193d5ef03fff4eabfb3c7a95929
iowa329 <iowa329@gmail.com>
2022-10-31 19:51:13 +0900

    10주차 금요일 백준14425번 구현

---
+++ practice/src/baekjoon/Baekjoon14425.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon14425 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer strTkr = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(strTkr.nextToken());
+		int m = Integer.parseInt(strTkr.nextToken());
+		
+		HashMap<String, Integer> hashMap = new HashMap<>();
+		
+		for(int i=0; i<n; i++) {
+			hashMap.put(br.readLine(), 0);	
+		}
+		
+		int answer = 0;
+		for(int i=0; i<m; i++) {
+			if(hashMap.get(br.readLine()) != null) answer++;
+		}
+		br.close();
+		
+		System.out.println(answer);
+	}
+
+}
