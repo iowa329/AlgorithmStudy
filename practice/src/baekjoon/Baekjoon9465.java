47666ec121f948630029571cde056dee8dc7376d
iowa329 <iowa329@gmail.com>
2022-11-12 22:24:43 +0900

    12주차 수요일 백준9465번 구현

--- practice/src/baekjoon/Baekjoon9465.java
+++ practice/src/baekjoon/Baekjoon9465.java
@@ -10,50 +10,31 @@ public class Baekjoon9465 {
 		
 		int t = Integer.parseInt(br.readLine());
 		for(int cases=1; cases<=t; cases++) {
-			// 스티커 홀짝 합 
-			int[][] sumEveneOrOdd = new int[2][2];
-			
 			// 스티커 정보 입력받기
 			int n = Integer.parseInt(br.readLine());
-			int[][] stickers = new int[2][n];
+			int[][] stickers = new int[2][n+1]; // dp에서 계산식을 편리하게 하기 위해 0번째 인덱스를 공란처리하기 위해 n+1 크기의 배열 선언
 			for(int i=0; i<2; i++) {
 				StringTokenizer strTkr = new StringTokenizer(br.readLine());
-				for(int j=0; j<n; j++) {
+				for(int j=1; j<=n; j++) {
 					stickers[i][j] = Integer.parseInt(strTkr.nextToken());
-					
-					int evenOrOdd = j % 2;
-					sumEveneOrOdd[i][evenOrOdd] += stickers[i][j];
 				}
 			}
 
-			// 스티커를 떼는 경우의 수
-			int[] sums = new int[4];
-			sums[0] = sumEveneOrOdd[0][0] + sumEveneOrOdd[1][1];
-			sums[1] = sumEveneOrOdd[0][1] + sumEveneOrOdd[1][0];
+			// 스티커를 떼었을 때 누적 최대값을 저장할 dp배열 선언
+			int[][] dp = new int[2][n+1];
 			
-			int lastIndex = n-1;
-			int evenOrOdd = lastIndex % 2;
-//			sums[2] = sums[evenOrOdd] - stickers[evenOrOdd][lastIndex] - stickers[evenOrOdd][lastIndex-1] + stickers[evenOrOdd][lastIndex];
-//			sums[3] = sums[evenOrOdd] - stickers[evenOrOdd][lastIndex-1] - stickers[evenOrOdd][lastIndex] + stickers[evenOrOdd][lastIndex];
+			// 첫번째 인덱스의 스티커 최대값은 기본 스티커 값이므로
+			dp[0][1] = stickers[0][1];
+			dp[1][1] = stickers[1][1];
 			
-			if(lastIndex>=2) {
-				if(evenOrOdd == 0) {
-					sums[2] = sums[0] - stickers[0][lastIndex] - stickers[1][lastIndex-1] + stickers[1][lastIndex];
-					sums[3] = sums[1] - stickers[0][lastIndex-1] - stickers[1][lastIndex] + stickers[0][lastIndex];				
-				} else {
-					sums[2] = sums[0] - stickers[0][lastIndex-1] - stickers[1][lastIndex] + stickers[0][lastIndex];
-					sums[3] = sums[1] - stickers[0][lastIndex] - stickers[1][lastIndex-1] + stickers[1][lastIndex];
-				}
+			// 두번째 인덱스부터 최대값 탐색 시작
+			for(int j=2; j<=n; j++) {
+				dp[0][j] = Math.max(dp[1][j-2], dp[1][j-1]) + stickers[0][j];
+				dp[1][j] = Math.max(dp[0][j-2], dp[0][j-1]) + stickers[1][j];
 			}
 			
-			
-			
-			// 최대값 찾기
-			int max = 0;
-			for(int sum: sums) max = Math.max(max, sum);
-			for(int sum: sums) System.out.println(sum);
-			
-			System.out.println(max);
+			// 누적된 최대값을 저장하는 배열이 dp이므로 2~n까지 탐색했을 때 스티커를 떼는 최대값은 dp배열에서 마지막인 n번째 인덱스에 있다
+			System.out.println(Math.max(dp[0][n], dp[1][n]));
 		}
 		br.close();
 	}
