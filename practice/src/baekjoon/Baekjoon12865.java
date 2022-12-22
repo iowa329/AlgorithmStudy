b8240daf27a5d9be95a64e60e9f9e74c892f5d4e
iowa329 <iowa329@gmail.com>
2022-12-22 20:42:29 +0900

    18주차 수요일 백준12865번 (참조)구현

--- practice/src/baekjoon/Baekjoon12865.java
+++ practice/src/baekjoon/Baekjoon12865.java
@@ -4,56 +4,97 @@ import java.io.*;
 import java.util.*;
 
 public class Baekjoon12865 {
-
-	static int n, k;
-	static int[][] stuff;
-	static boolean[] combi;
-	static int maxValue = 0;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer strTkr = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(strTkr.nextToken());
-		k = Integer.parseInt(strTkr.nextToken());
+		int n = Integer.parseInt(strTkr.nextToken());
+		int k = Integer.parseInt(strTkr.nextToken());
 	
-		stuff = new int[n][2];
-		int totalWeight = 0;
-		for(int i=0; i<n; i++) {
+		int[] w = new int[n+1]; // 무게 w
+		int[] v = new int[n+1]; // 가치 v
+		for(int i=1; i<=n; i++) {
 			strTkr = new StringTokenizer(br.readLine());
-			stuff[i][0] = Integer.parseInt(strTkr.nextToken()); // 무게 w
-			stuff[i][1] = Integer.parseInt(strTkr.nextToken()); // 가치 v
-			
-			totalWeight += stuff[i][0];
+			w[i] = Integer.parseInt(strTkr.nextToken());
+			v[i] = Integer.parseInt(strTkr.nextToken());
 		}
 		br.close();
 		
-		combi = new boolean[n];
-		Arrays.fill(combi, true);
-		
-		dfs(0, totalWeight);
-		
-		System.out.println(maxValue);
-	}
-	
-	private static void dfs(int startIndex, int weight) {
-		if(weight <= k) {
-			int totalValue = 0;
-			for(int i=0; i<n; i++) {
-				if(combi[i]) {
-					totalValue += stuff[i][1];
+		int[][] dp = new int[k+1][n+1]; // 넣을 수 있는 무게의 최대 가치값 dp배열 선언
+		for(int serachWeight=1; serachWeight<=k; serachWeight++) { // 버틸 수 있는 최대 무게 k
+			for(int searchIndex=1; searchIndex<=n; searchIndex++) { // 탐색할 물건 1~n개까지
+				int curIndexWeight = w[searchIndex];
+				int curIndexValue = v[searchIndex];
+				int beforeIndexValue = dp[serachWeight][searchIndex-1];
+				// 무게를 더 담을 수 없는 경우
+				if(curIndexWeight > serachWeight) {
+					dp[serachWeight][searchIndex] = beforeIndexValue;
+				} else {
+					// 무게를 더 담을 수 있는 경우
+					dp[serachWeight][searchIndex] = Math.max(beforeIndexValue, curIndexValue + dp[serachWeight-curIndexWeight][searchIndex-1]);
 				}
 			}
-			maxValue = Math.max(maxValue, totalValue);
-			
-			return;
 		}
 		
-		for(int i=startIndex; i<n; i++) {
-			if(combi[i] == true) {
-				combi[i] = false;
-				dfs(i+1, weight - stuff[i][0]);
-				combi[i] = true;
-			}
-		}
+		// 최대 k무게를 버티는 n번째 물품까지의 탐색 결과의 최대값 출력
+		System.out.println(dp[k][n]);
 	}
+	
 }
+
+// 구현1 DFS (시간초과)
+
+//public class Baekjoon12865 {
+//
+//	static int n, k;
+//	static int[][] stuff;
+//	static boolean[] combi;
+//	static int maxValue = 0;
+//	
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer strTkr = new StringTokenizer(br.readLine());
+//		n = Integer.parseInt(strTkr.nextToken());
+//		k = Integer.parseInt(strTkr.nextToken());
+//	
+//		stuff = new int[n][2];
+//		int totalWeight = 0;
+//		for(int i=0; i<n; i++) {
+//			strTkr = new StringTokenizer(br.readLine());
+//			stuff[i][0] = Integer.parseInt(strTkr.nextToken()); // 무게 w
+//			stuff[i][1] = Integer.parseInt(strTkr.nextToken()); // 가치 v
+//			
+//			totalWeight += stuff[i][0];
+//		}
+//		br.close();
+//		
+//		combi = new boolean[n];
+//		Arrays.fill(combi, true);
+//		
+//		dfs(0, totalWeight);
+//		
+//		System.out.println(maxValue);
+//	}
+//	
+//	private static void dfs(int startIndex, int weight) {
+//		if(weight <= k) {
+//			int totalValue = 0;
+//			for(int i=0; i<n; i++) {
+//				if(combi[i]) {
+//					totalValue += stuff[i][1];
+//				}
+//			}
+//			maxValue = Math.max(maxValue, totalValue);
+//			
+//			return;
+//		}
+//		
+//		for(int i=startIndex; i<n; i++) {
+//			if(combi[i] == true) {
+//				combi[i] = false;
+//				dfs(i+1, weight - stuff[i][0]);
+//				combi[i] = true;
+//			}
+//		}
+//	}
+//}
