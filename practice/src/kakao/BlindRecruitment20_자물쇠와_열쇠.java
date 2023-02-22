aa49a0c0bc9434e48ee97f75338a9150b00b12ee
iowa329 <iowa329@gmail.com>
2023-02-23 02:31:45 +0900

    26주차 수요일 카카오블라인드20 자물쇠와 열쇠 구현

--- practice/src/kakao/BlindRecruitment20_자물쇠와_열쇠.java
+++ practice/src/kakao/BlindRecruitment20_자물쇠와_열쇠.java
@@ -1,14 +1,6 @@
 package kakao;
 
-import java.util.*;
-
 public class BlindRecruitment20_자물쇠와_열쇠 {
-
-	private static void printArrays(int[][] targetArray) {
-		for(int[] array: targetArray) {
-			System.out.println(Arrays.toString(array));
-		}
-	}
 	
 	public static void main(String[] args) {
 		int[][] key = { { 0, 0, 0 },
@@ -22,22 +14,19 @@ public class BlindRecruitment20_자물쇠와_열쇠 {
 	}
 	
 	static int keySize;
+	static int lockSize;
 	
 	public static boolean solution(int[][] key, int[][] lock) {
 		keySize = key.length;
-		int expandLockSize = lock.length + (keySize * 2) - 2;
+		lockSize = lock.length;
+		int expandLockSize = lockSize + ((keySize-1) * 2);
 		
 		// lock을 중심에 둔 확장된 lock배열 선언
-		// 경계 밖 부분 -1로 초기화
 		int[][] expandedLock = new int[expandLockSize][expandLockSize];
-		for(int[] expandedLockX: expandedLock) {
-			Arrays.fill(expandedLockX, -1);
-		}
-		
 		// 중심에 lock 배치
 		int startIndex = keySize-1;
-		for(int x=startIndex; x<startIndex+keySize; x++) {
-			for(int y=startIndex; y<startIndex+keySize; y++) {
+		for(int x=startIndex; x<startIndex+lockSize; x++) {
+			for(int y=startIndex; y<startIndex+lockSize; y++) {
 				expandedLock[x][y] = lock[x-startIndex][y-startIndex];
 			}
 		}
@@ -45,27 +34,39 @@ public class BlindRecruitment20_자물쇠와_열쇠 {
 		boolean answer = false;
 		
 		// key를 돌려가며 완전탐색
+		loop_Rotation:
 		for(int rotation=0; rotation<=270; rotation+=90) {
 			
-			for(int startX=0; startX<keySize-1+lock.length; startX++) {
-				for(int startY=0; startY<keySize-1+lock.length; startY++) {
+			// 처음(0도)를 제외하고 key 회전
+			if(rotation != 0)
+				key = rotateKey(key);
+			
+			for(int startX=0; startX<keySize-1+lockSize; startX++) {
+				loop_checkItCanBeUnlock:
+				for(int startY=0; startY<keySize-1+lockSize; startY++) {
 					
-					for(int x=startX; x<keySize; x++) {
-						for(int y=startY; y<keySize; y++) {
-							if(expandedLock[x][y] == -1)
-								continue;
-							if(expandedLock[x][y])
-							
+					// expandLock배열 크기와 동일한 확장된 key배열 선언
+					int[][] expandedKey = new int[expandLockSize][expandLockSize];
+					for(int x=startX; x<startX+keySize; x++)
+						for(int y=startY; y<startY+keySize; y++)
+							expandedKey[x][y] = key[x-startX][y-startY];
+					
+					// expandedLock 중간에 있는 lock 기준으로 잠금해제 가능 여부검사
+					for(int x=startIndex; x<startIndex+lockSize; x++) {
+						for(int y=startIndex; y<startIndex+lockSize; y++) {
+							// lock과 key의 합이 1이 아니면 잠금해제를 할 수 없다
+							if(expandedLock[x][y] + expandedKey[x][y] != 1) { // 잠금해제를 할 수 없는 경우
+								continue loop_checkItCanBeUnlock; // 조건검사 반복문 종료
+							}
 						}
-					}					
+					}
+					
+					// 여기까지 온 경우 반례가 없어 잠금해제를 할 수 있다는 뜻이므로
+					answer = true; // 가능처리 후
+					break loop_Rotation; // 전체 탐색 완전종료
 					
 				}
 			}
-			
-			
-				
-			
-			key = rotateKey(key);
 			
 		}
 		
